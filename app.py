162d888319da22f66fef1eb0f9dabf0d78a35eb9
Seano <sean01234@yahoo.com>
2023-10-05 16:18:28 -0400

    install flask, gunicorn, create requirements.txt

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Deply Me!</h1><style>body { display: flex; align-items: center; justify-content:center; height: 100vh;}'
